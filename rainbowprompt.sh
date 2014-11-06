9b5f74319f4f43bc8f8c3f4d626220421e85549f
Eric Richter <richteer@clarkson.edu>
2014-11-05 20:00:28 -0500

    Fixed some things purely for my own convenience

--- rainbowprompt.sh
+++ rainbowprompt.sh
@@ -1,6 +1,6 @@
 # Define your own colors here. Be sure to update line 14 with a new modulus
 # if you add more colors
-PROMPT_COLOR=6
+PROMPT_COLOR=5
 color[0]="\e[0;31m"
 color[1]="\e[0;33m"
 color[2]="\e[0;32m"
@@ -9,7 +9,7 @@ color[4]="\e[0;34m"
 color[5]="\e[0;35m"
 
 PROMPT_RESET="\e[0m"
-PS1='\u@\h:\w\$ ' # Change this to your desired prompt w/o color codes
+#PS1='\u@\h:\w\$ ' # Change this to your desired prompt w/o color codes
 DPS1=$PS1
 
 function rotate_color() {

c0a754c852b7f90cb7236a2310dc7532669bf4d1
Eric Richter <richteer@clarkson.edu>
2014-11-26 13:58:36 -0500

    Finally fixed that stupid wrapping bug

--- rainbowprompt.sh
+++ rainbowprompt.sh
@@ -1,14 +1,14 @@
 # Define your own colors here. Be sure to update line 14 with a new modulus
 # if you add more colors
 PROMPT_COLOR=5
-color[0]="\e[0;31m"
-color[1]="\e[0;33m"
-color[2]="\e[0;32m"
-color[3]="\e[0;36m"
-color[4]="\e[0;34m"
-color[5]="\e[0;35m"
+color[0]="\[\e[0;31m\]"
+color[1]="\[\e[0;33m\]"
+color[2]="\[\e[0;32m\]"
+color[3]="\[\e[0;36m\]"
+color[4]="\[\e[0;34m\]"
+color[5]="\[\e[0;35m\]"
 
-PROMPT_RESET="\e[0m"
+PROMPT_RESET="\[\e[0m\]"
 #PS1='\u@\h:\w\$ ' # Change this to your desired prompt w/o color codes
 DPS1=$PS1
 
